dca96074f0dfa329ab733eb68abbcae345780c1f
Aleksandre Naneishvili <aleksandrenaneishvili@gmail.com>
2024-04-05 17:10:25 +0400

    modifications to main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {join} from "path";
+import {NestExpressApplication} from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+  });
+  app.enableCors();
+  app.useStaticAssets(join(__dirname, '..', 'files'), {
+    prefix: '/files/',
+  });
+
+  await app.listen(process.env.PORT || 5000, '0.0.0.0');
 }
 bootstrap();
