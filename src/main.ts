9a6df20cfda8221140c1d05acc4c5e4c52e82b8c
Aleksandre Naneishvili <aleksandrenaneishvili@gmail.com>
2024-05-01 17:11:27 +0400

    modified cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
-  app.enableCors();
+  app.enableCors({
+    origin: 'https://d35dh7a9lkzan1.cloudfront.net', // Frontend domain
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // HTTP methods to allow
+    credentials: true, // Allow credentials if needed
+  });
   app.useStaticAssets(join(__dirname, '..', 'files'), {
     prefix: '/files/',
   });
