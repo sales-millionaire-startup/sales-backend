415fc64caebc9bac1f8dc99579df2f032b12c5a3
Aleksandre Naneishvili <aleksandrenaneishvili@gmail.com>
2024-05-06 17:22:04 +0400

    modified cors to all origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {});
   app.enableCors({
-    origin: 'https://d35dh7a9lkzan1.cloudfront.net', // Frontend domain
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // HTTP methods to allow
     credentials: true, // Allow credentials if needed
   });
